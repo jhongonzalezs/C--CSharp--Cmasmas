40acdc8fb464738815fdab5e13f0f38c0a1683c4
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2024-02-03 22:37:17 -0500

    Ejercicio basico en  explicado cuando un numero es par o impar.

---
+++ par_Impar/par_Impar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace par_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, C_Numeros;
+
+            Console.WriteLine("Digite Una La cantidad");
+            C_Numeros = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= C_Numeros; i++) {
+                
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Par" + " (" + i+") ");
+                }
+                else
+                {
+                    if (i%2 != 0)
+                    {
+                        Console.WriteLine("Impar" + " (" + i + ") ");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
